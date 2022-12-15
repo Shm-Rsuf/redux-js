eef4645f2e32e3729bdceab5d8a9306cf64cc162
shm_usuf <shmusuf@gmail.com>
2022-12-15 20:58:33 +0600

    redux intro

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello from redux");
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const BUY_PASTA = "BUY_PASTA";
+function buyPasta() {
+  return {
+    type: BUY_PASTA,
+    quantity: 10,
+  };
+}
+
+const initialState = {
+  numOfPasta: 20,
+  numOfPizza: 40,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_PASTA:
+      return { ...state, numOfPasta: state.numOfPasta - 1 };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const unsubcribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(buyPasta());
+unsubcribe();
